54ab1ec1e8a96a563c3ee20aecdfbd35f954a615
MarcoAguirre <viniciosoria16@gmail.com>
2024-09-25 00:56:52 -0500

    Add recommendation feature
    - Now the AI can recommend resources which can come from any resource
    - Delete old function to read files and use the content as context
    - Delete old markdown file since it is not necessary anymore

--- backend/main.py
+++ backend/main.py
@@ -6,6 +6,8 @@ from openai import OpenAI
 from dotenv import load_dotenv
 import os
 
+from topics_to_files import topic_to_file
+
 load_dotenv()
 
 app = FastAPI()
@@ -25,29 +27,25 @@ client = OpenAI(
 class PromptRequest(BaseModel):
     prompt: str
 
-def get_context_from_file():
-    try:
-        with open("Branching strategies.md", "r") as file:
-            context = file.read()
-        return context
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Error reading file: {str(e)}")
-
 @app.get("/")
 def read_root():
     return {"message": "Hello World"}
 
 @app.post("/chat/")
 async def chat_with_gpt(request: PromptRequest):
+    query = f"""
+        Identify key topics in the following feedback comment: '{request.prompt}'
+        and recommend me some files to read and improve on those key topics.
+        Right now the available files are: '{topic_to_file}'.
+        Please return the response in markdown format.
+        """
     try:
-        context = get_context_from_file()
         response = client.chat.completions.create(
             model= "gpt-4o-mini",
             messages=[
-                {"role": "system", "content": context},
                 {
                     "role": "user",
-                    "content": request.prompt
+                    "content": query
                 }
             ]
         )
---
+++ backend/topics_to_files.py
@@ -0,0 +1,6 @@
+topic_to_file = {
+    "clean code": "https://notion.so/clean_code_resources_link",
+    "data types": "https://notion.so/data_types_resources_link",
+    "branching strategies": "https://notion.so/branching_strategies_link",
+    "testing": "https://notion.so/testing_resources_link",
+}
